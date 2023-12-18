819d9c91aba3de5a60fa310408d84c1bdd9da303
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-12-18 19:17:39 +0200

    before challenges

--- Project29/GameScene.swift
+++ Project29/GameScene.swift
@@ -164,7 +164,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 self.changePlayer()
                 newGame.currentPlayer = self.currentPlayer
                 
-                let transition = SKTransition.doorway(withDuration: 1.5)
+                let transition = SKTransition.doorway(withDuration: 4.5)
                 self.view?.presentScene(newGame, transition: transition)
             }
         }
